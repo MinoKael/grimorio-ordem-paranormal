a02c373e5312cba36bb8e756f479f0f58904b61c
Mikael Mota <mikael.mota@localize.com.br>
2025-01-03 13:21:06 -0300

    feat: adicionado direitos reservados

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,14 +4,19 @@
     <v-footer app bottom absolute padless>
         <v-col>
             <v-row align="center" justify="center" no-gutters>
-                2025 — <span class="text-button">MinoKael</span>
+                2025 — <span class="text-button ml-1"> MinoKael</span>
                 <v-btn density="compact" class="ml-2" rounded="xl" icon="mdi-github"
                     href="https://github.com/MinoKael/grimorio-ordem-paranormal" target="_blank">
                 </v-btn>
                 <v-btn density="compact" class="ml-2" rounded="xl" icon="mdi-twitter" href="https://x.com/MinoKael/" target="_blank">
                 </v-btn>
             </v-row>
-            <v-row align="baseline" justify="center" no-gutters>
+            <v-row align="baseline" justify="center" no-gutters class="text-caption">
+                Ordem Paranormal é um produto de 
+                <v-btn density="compact" variant="text" class="px-1" rounded="xl"  href="https://jamboeditora.com.br/" target="_blank">
+                    Jambô Editora
+                </v-btn>
+                e seus respectivos criadores, todos os direitos reservados. Este é um projeto de fã e não possui fins lucrativos. 
             </v-row>
         </v-col>
     </v-footer>
