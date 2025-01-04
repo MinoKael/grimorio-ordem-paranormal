fc8d947c5d296f9ebc56c5142e048f717d573e17
Mikael Mota <mikael.mota@localize.com.br>
2025-01-03 22:37:21 -0300

    fix: caminho absoluto

--- src/plugins/global.js
+++ src/plugins/global.js
@@ -13,6 +13,6 @@ function elementoCor(elemento) {
     }
 }
 function getImageUrl(image) {
-    return new URL(`/assets/${image}`, import.meta.url).href;
+    return `/assets/${image}`;
 }
 export { elementoCor, getImageUrl }
