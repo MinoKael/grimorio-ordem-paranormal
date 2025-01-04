62cc27714fc78e46005bd3447fef3ea9a9c86981
Mikael Mota <mikael.mota@localize.com.br>
2025-01-04 00:05:39 -0300

    fix: alterado close dialog

--- src/components/DialogSimbolo.vue
+++ src/components/DialogSimbolo.vue
@@ -10,10 +10,7 @@ const dialog = ref(false);
 </script>
 <template>
     <v-dialog v-model="dialog" height="100%" scrim="black" opacity="1" activator="parent">
-        <v-btn flat icon @click="dialog = !dialog" class="align-self-center">
-            <v-icon>mdi-close</v-icon>
-        </v-btn>
-        <div class="image-container">
+        <div class="image-container" @click="dialog = !dialog">
             <v-img height="100%" aspect-ratio="4/3" :src="src" :alt="alt" style="margin: 0;" />
         </div>
     </v-dialog>
