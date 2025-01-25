35459d782f95ea03b38e8d3862dc338982408c8d
Mikael Mota <mikael.mota@localize.com.br>
2025-01-25 19:52:42 -0300

    fix: dialog de ritual overflow

--- src/components/DialogCard.vue
+++ src/components/DialogCard.vue
@@ -11,31 +11,25 @@ const dialog = ref(false);
 const imageNotLoaded = ref(false);
 const { ritualIndex, rituals } = defineProps(['ritualIndex', 'rituals']);
 
-let ritual = Object.assign({}, rituals[ritualIndex]);
-let currentIndex = ref(ritualIndex);
-let imageUrl = getImageUrl(ritual.simbolo);
+const currentRitual = computed(() => rituals[currentIndex.value]);
+const currentIndex = ref(ritualIndex);
+const imageUrl = computed(() => getImageUrl(currentRitual.value.simbolo));
 
 function nextRitual() {
     if (currentIndex.value < rituals.length - 1) {
         currentIndex.value++;
         imageNotLoaded.value = false;
-        ritual = rituals[currentIndex.value];
-        imageUrl = getImageUrl(ritual.simbolo);
     }
 }
 function prevRitual() {
     if (currentIndex.value > 0) {
         currentIndex.value--;
         imageNotLoaded.value = false;
-        ritual = rituals[currentIndex.value];
-        imageUrl = getImageUrl(ritual.simbolo);
     }
 }
 watch(dialog, () => {
     if(dialog.value) {
         currentIndex.value = ritualIndex;
-        ritual = Object.assign({}, rituals[ritualIndex]);
-        imageUrl = getImageUrl(ritual.simbolo);
     }
 });
 </script>
@@ -49,89 +43,87 @@ watch(dialog, () => {
         activator="parent"
         scrim="black"
     >
-        <div class="d-flex flex-row align-center justify-center">
-            <v-btn icon @click="prevRitual" :disabled="currentIndex === 0">
-                <v-icon>mdi-arrow-left</v-icon>
-            </v-btn>
-            <v-card class="mx-2">
-                <v-row no-gutters>
-                    <v-col no-gutters cols="7">
-                        <v-card-title class="text-h4 font-weight-bold text-tormentaText text-wrap px-6 pt-4">
-                            {{ ritual.nome }}
-                        </v-card-title>
-                        <v-card-subtitle class="d-flex align-center font-weight-bold px-6">
-                            <span
-                                class="text-h6 px-1"
-                                :class="ritual.elemento === 'Medo' ? 'font-weight-light' : 'font-weight-bold '"
-                                :style="{
-                                    'background-color': elementoCor(ritual.elemento),
-                                    color: ritual.elemento === 'Medo' ? 'black' : 'white'
-                                }">
-                                {{ ritual.elemento }} {{ ritual.circulo }}
-                            </span>
-                            <span class="text-h6 font-weight-bold ml-1">
-                                {{ ritual.custo }} PE
-                            </span>
-                        </v-card-subtitle>
-                        <v-card-text class="d-flex flex-column px-6 text-body-1">
-                            <span v-if="ritual.execucao">
-                                <span class="text-tormentaText"><strong>Execução:</strong> </span> {{ ritual.execucao }};&nbsp
-                            </span>
-                            <span v-if="ritual.alcance">
-                                <span class="text-tormentaText"><strong>Alcance:</strong> </span> {{ ritual.alcance }};&nbsp
-                            </span>
-                            <span v-if="ritual.alvo">
-                                <span class="text-tormentaText"><strong>Alvo:</strong> </span> {{ ritual.alvo }};&nbsp
-                            </span>
-                            <span v-if="ritual.area">
-                                <span class="text-tormentaText"><strong>Área:</strong> </span> {{ ritual.area }};&nbsp
-                            </span>
-                            <span v-if="ritual.efeito">
-                                <span class="text-tormentaText"><strong>Efeito:</strong> </span> {{ ritual.efeito }};&nbsp
-                            </span>
-                            <span v-if="ritual.duracao">
-                                <span class="text-tormentaText"><strong>Duração:</strong> </span> {{ ritual.duracao }};&nbsp
-                            </span>
-                            <span v-if="ritual.resistencia">
-                                <span class="text-tormentaText"><strong>Resistência:</strong> </span> {{ ritual.resistencia }};&nbsp
-                            </span>
-                            <span>
-                                <span class="text-tormentaText"><strong>Referência:</strong> </span> {{ ritual.referencia }};&nbsp
-                            </span>
-                        </v-card-text>
-                    </v-col>
-                    <v-col no-gutters cols="5" class="d-flex align-center justify-center">
-                        <div class="d-flex justify-center align-center pr-3">
-                            <img height="100%" :src="imageUrl" :alt="`Símbolo ${ritual.nome}`" class="simbolo" :class="{ 'image-error': imageNotLoaded }" @error="imageNotLoaded = true">
-                                <DialogSimbolo v-if="!imageNotLoaded" :src="imageUrl" :alt="`Símbolo ${ritual.nome}`" />
-                            </img>
-                        </div>
-                    </v-col>
-                </v-row>
-                <v-card-text class="pt-0 text-body-1">
-                    <v-divider></v-divider>
-    
-                    <p class="mt-2 text-pre-wrap">{{ ritual.texto }}</p>
-    
-                    <v-divider class="my-3"></v-divider>
-    
-                    <p v-for="aprimoramento in ritual.aprimoramentos">
-                        <span class="text-tormentaText font-weight-bold">
-                            {{ aprimoramento.match(/.+?:/).toString() }}
+        <v-card class="mx-2">
+            <v-row no-gutters>
+                <v-col no-gutters cols="7">
+                    <v-card-title class="text-h4 font-weight-bold text-tormentaText text-wrap px-6 pt-4">
+                        {{ currentRitual.nome }}
+                    </v-card-title>
+                    <v-card-subtitle class="d-flex align-center font-weight-bold px-6">
+                        <span
+                        class="text-h6 px-1"
+                        :class="currentRitual.elemento === 'Medo' ? 'font-weight-light' : 'font-weight-bold '"
+                        :style="{
+                                'background-color': elementoCor(currentRitual.elemento),
+                                color: currentRitual.elemento === 'Medo' ? 'black' : 'white'
+                            }">
+                            {{ currentRitual.elemento }} {{ currentRitual.circulo }}
                         </span>
-                        {{ aprimoramento.match(/(?<=:).+/).toString() }}
-                    </p>
-                </v-card-text>
-                <v-card-actions>
-                    <v-btn color="tormentaText" block @click="dialog = false">
-                        Fechar
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-            <v-btn icon @click="nextRitual" :disabled="currentIndex === rituals.length - 1">
-                <v-icon>mdi-arrow-right</v-icon>
-            </v-btn>
-         </div>
+                        <span class="text-h6 font-weight-bold ml-1">
+                            {{ currentRitual.custo }} PE
+                        </span>
+                    </v-card-subtitle>
+                    <v-card-text class="d-flex flex-column px-6 text-body-1">
+                        <span v-if="currentRitual.execucao">
+                            <span class="text-tormentaText"><strong>Execução:</strong> </span> {{ currentRitual.execucao }};&nbsp
+                        </span>
+                        <span v-if="currentRitual.alcance">
+                            <span class="text-tormentaText"><strong>Alcance:</strong> </span> {{ currentRitual.alcance }};&nbsp
+                        </span>
+                        <span v-if="currentRitual.alvo">
+                            <span class="text-tormentaText"><strong>Alvo:</strong> </span> {{ currentRitual.alvo }};&nbsp
+                        </span>
+                        <span v-if="currentRitual.area">
+                            <span class="text-tormentaText"><strong>Área:</strong> </span> {{ currentRitual.area }};&nbsp
+                        </span>
+                        <span v-if="currentRitual.efeito">
+                            <span class="text-tormentaText"><strong>Efeito:</strong> </span> {{ currentRitual.efeito }};&nbsp
+                        </span>
+                        <span v-if="currentRitual.duracao">
+                            <span class="text-tormentaText"><strong>Duração:</strong> </span> {{ currentRitual.duracao }};&nbsp
+                        </span>
+                        <span v-if="currentRitual.resistencia">
+                            <span class="text-tormentaText"><strong>Resistência:</strong> </span> {{ currentRitual.resistencia }};&nbsp
+                        </span>
+                        <span>
+                            <span class="text-tormentaText"><strong>Referência:</strong> </span> {{ currentRitual.referencia }};&nbsp
+                        </span>
+                    </v-card-text>
+                </v-col>
+                <v-col no-gutters cols="5" class="d-flex align-center justify-center">
+                    <div class="d-flex justify-center align-center pr-3">
+                        <img height="100%" :src="imageUrl" :alt="`Símbolo ${currentRitual.nome}`" class="simbolo" :class="{ 'image-error': imageNotLoaded }" @error="imageNotLoaded = true">
+                        <DialogSimbolo v-if="!imageNotLoaded" :src="imageUrl" :alt="`Símbolo ${currentRitual.nome}`" />
+                    </img>
+                </div>
+            </v-col>
+            </v-row>
+            <v-card-text class="pt-0 pb-2 text-body-1">
+                <v-divider></v-divider>
+                
+                <p class="mt-2 text-pre-wrap">{{ currentRitual.texto }}</p>
+                
+                <v-divider class="my-3"></v-divider>
+                
+                <p v-for="aprimoramento in currentRitual.aprimoramentos">
+                    <span class="text-tormentaText font-weight-bold">
+                        {{ aprimoramento.match(/.+?:/).toString() }}
+                    </span>
+                    {{ aprimoramento.match(/(?<=:).+/).toString() }}
+                </p>
+            </v-card-text>
+            <v-card-actions class="justify-space-between">
+                <v-btn icon @click="prevRitual" :disabled="currentIndex === 0">
+                    <v-icon>mdi-arrow-left</v-icon>
+                </v-btn>
+                <v-btn color="tormentaText" style="width: 80%;" @click="dialog = false">
+                    Fechar
+                </v-btn>
+                <v-btn icon @click="nextRitual" :disabled="currentIndex === rituals.length - 1">
+                    <v-icon>mdi-arrow-right</v-icon>
+                </v-btn>
+            </v-card-actions>
+        </v-card>
     </v-dialog>
     <v-dialog
         v-else
@@ -144,61 +136,61 @@ watch(dialog, () => {
     >
         <v-card>
             <v-card-title class="text-h4 font-weight-bold text-tormentaText text-wrap px-6 pt-4 pb-0">
-                {{ ritual.nome }}
+                {{ currentRitual.nome }}
             </v-card-title>
             <span class="d-flex justify-center">
-                <img v-if="!imageNotLoaded" height="100%" :src="imageUrl" :alt="`Símbolo ${ritual.nome}`" class="simbolo" :class="{ 'image-error': imageNotLoaded }" @error="imageNotLoaded = true">
-                    <DialogSimbolo :src="imageUrl" :alt="`Símbolo ${ritual.nome}`" />
+                <img v-if="!imageNotLoaded" height="100%" :src="imageUrl" :alt="`Símbolo ${currentRitual.nome}`" class="simbolo" :class="{ 'image-error': imageNotLoaded }" @error="imageNotLoaded = true">
+                    <DialogSimbolo :src="imageUrl" :alt="`Símbolo ${currentRitual.nome}`" />
                 </img>
             </span>
             <v-card-subtitle class="d-flex align-center font-weight-bold px-6">
                 <span
                     class="text-h6 px-1"
-                    :class="ritual.elemento === 'Medo' ? 'font-weight-light' : 'font-weight-bold '"
+                    :class="currentRitual.elemento === 'Medo' ? 'font-weight-light' : 'font-weight-bold '"
                     :style="{
-                        'background-color': elementoCor(ritual.elemento),
-                        color: ritual.elemento === 'Medo' ? 'black' : 'white'
+                        'background-color': elementoCor(currentRitual.elemento),
+                        color: currentRitual.elemento === 'Medo' ? 'black' : 'white'
                     }">
-                    {{ ritual.elemento }} {{ ritual.circulo }}
+                    {{ currentRitual.elemento }} {{ currentRitual.circulo }}
                 </span>
                 <span class="ml-2 text-h6 font-weight-bold">
-                    {{ ritual.custo }} PE
+                    {{ currentRitual.custo }} PE
                 </span>
             </v-card-subtitle>
             <div class="d-flex flex-column text-body-1 pl-6">
-                <span v-if="ritual.execucao">
-                    <span class="text-tormentaText"><strong>Execução:</strong> </span> {{ ritual.execucao }};&nbsp
+                <span v-if="currentRitual.execucao">
+                    <span class="text-tormentaText"><strong>Execução:</strong> </span> {{ currentRitual.execucao }};&nbsp
                 </span>
-                <span v-if="ritual.alcance">
-                    <span class="text-tormentaText"><strong>Alcance:</strong> </span> {{ ritual.alcance }};&nbsp
+                <span v-if="currentRitual.alcance">
+                    <span class="text-tormentaText"><strong>Alcance:</strong> </span> {{ currentRitual.alcance }};&nbsp
                 </span>
-                <span v-if="ritual.alvo">
-                    <span class="text-tormentaText"><strong>Alvo:</strong> </span> {{ ritual.alvo }};&nbsp
+                <span v-if="currentRitual.alvo">
+                    <span class="text-tormentaText"><strong>Alvo:</strong> </span> {{ currentRitual.alvo }};&nbsp
                 </span>
-                <span v-if="ritual.area">
-                    <span class="text-tormentaText"><strong>Área:</strong> </span> {{ ritual.area }};&nbsp
+                <span v-if="currentRitual.area">
+                    <span class="text-tormentaText"><strong>Área:</strong> </span> {{ currentRitual.area }};&nbsp
                 </span>
-                <span v-if="ritual.efeito">
-                    <span class="text-tormentaText"><strong>Efeito:</strong> </span> {{ ritual.efeito }};&nbsp
+                <span v-if="currentRitual.efeito">
+                    <span class="text-tormentaText"><strong>Efeito:</strong> </span> {{ currentRitual.efeito }};&nbsp
                 </span>
-                <span v-if="ritual.duracao">
-                    <span class="text-tormentaText"><strong>Duração:</strong> </span> {{ ritual.duracao }};&nbsp
+                <span v-if="currentRitual.duracao">
+                    <span class="text-tormentaText"><strong>Duração:</strong> </span> {{ currentRitual.duracao }};&nbsp
                 </span>
-                <span v-if="ritual.resistencia">
-                    <span class="text-tormentaText"><strong>Resistência:</strong> </span> {{ ritual.resistencia }};&nbsp
+                <span v-if="currentRitual.resistencia">
+                    <span class="text-tormentaText"><strong>Resistência:</strong> </span> {{ currentRitual.resistencia }};&nbsp
                 </span>
                 <span>
-                    <span class="text-tormentaText"><strong>Referência:</strong> </span> {{ ritual.referencia }};&nbsp
+                    <span class="text-tormentaText"><strong>Referência:</strong> </span> {{ currentRitual.referencia }};&nbsp
                 </span>
             </div>
             <v-divider class="my-3"></v-divider>
             <v-card-text class="pt-2">
 
-                <p class="text-pre-wrap">{{ ritual.texto }}</p>
+                <p class="text-pre-wrap">{{ currentRitual.texto }}</p>
 
                 <v-divider class="my-3"></v-divider>
 
-                <p v-for="aprimoramento in ritual.aprimoramentos">
+                <p v-for="aprimoramento in currentRitual.aprimoramentos">
                     <span class="text-tormentaText font-weight-bold">{{
                         aprimoramento.match(/.+?:/).toString()
                     }}</span>
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,17 @@ import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
 // https://vite.dev/config/
-export default defineConfig({
-    base: '/grimorio-ordem-paranormal/',
-    plugins: [
-        vue({
-            template: { transformAssetUrls},
-        }),
-        vuetify({
-            autoImport: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {    
+    const isProduction = mode === 'production';
+    return {
+        base: isProduction ? '/grimorio-ordem-paranormal/' : '/',
+        plugins: [
+            vue({
+                template: { transformAssetUrls},
+            }),
+            vuetify({
+                autoImport: true,
+            }),
+        ],
+    }
 })
